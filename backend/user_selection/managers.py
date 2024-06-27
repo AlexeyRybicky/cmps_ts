43a469bd6eba62c8e7f844788f44e25b480b7924
Aleksey <ranangariel@gmail.com>
2024-06-27 19:32:12 +0300

    Добавил менеджера для создания пользователей

---
+++ backend/user_selection/managers.py
@@ -0,0 +1,25 @@
+"""
+В модуле содержится класс UserManager, представляет менеджер пользователей Django для создания обычных пользователей
+и суперпользователей.
+"""
+
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        """Создает обычного пользователя"""
+        if not username:
+            raise ValueError('Users must have a username')
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        """Создает суперпользователя"""
+        user = self.create_user(username, email, password=password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
