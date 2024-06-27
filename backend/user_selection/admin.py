cd625ac2d05e7f57053049e79513a6c403f2e0db
Aleksey <ranangariel@gmail.com>
2024-06-27 19:34:17 +0300

    Добавил возможность взаимодействия с пользователями через админ панель

--- backend/user_selection/admin.py
+++ backend/user_selection/admin.py
@@ -1,3 +1,26 @@
 """В модуле содержаться классы для работы с админ панелью"""
 
 from django.contrib import admin
+from user_selection.models import User
+
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+    )
+    fields = (
+        'username',
+        'email',
+        'role',
+        'offer',
+        'avatar',
+        'password',
+        'is_superuser'
+    )
+    list_display_links = (
+        'id',
+        'username'
+    )
