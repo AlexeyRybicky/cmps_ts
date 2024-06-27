25c299e8b7678f0e1ce70d0ffddda61bbf88025f
Aleksey <ranangariel@gmail.com>
2024-06-27 19:31:33 +0300

    Добавил представления

--- backend/user_selection/views.py
+++ backend/user_selection/views.py
@@ -1,4 +1,23 @@
-"""В модуле содержаться представления"""
+"""В модуле содержаться представления для `user_selection` приложения"""
 
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+from .serializers import UserSerializer
 
+
+class UserDetailView(APIView):
+    """
+    Класс представления для получения информации о пользователе
+
+    Атрибуты:
+        pk: Целочисленный идентификатор пользователя.
+
+    Методы:
+        get: Получает информацию о пользователе с заданным идентификатором.
+    """
+
+    def get(self, request, pk):
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
